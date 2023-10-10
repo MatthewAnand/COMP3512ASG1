baa17881216be6a900439d51019a99c2952607ec
MatthewAnand <manan495@mtroyal.ca>
2023-10-10 10:15:27 -0600

    Update databasestuff.inc.php

--- includes/databasestuff.inc.php
+++ includes/databasestuff.inc.php
@@ -1,22 +1,5 @@
 <?php
 require_once('config.inc.php');
-function songId(){
-try{
-
-    $pdo= new PDO(DBCONNSTRING, DBUSER, DBPASS);
-    $sql="Select song_id FROM songs";
-    $sId = $pdo-> Query($sql);
-    echo "<ul>";
-    while($row = $sId ->fetch()){
-        echo "<li>". $row['song_id'] ."</li>";
-    }
-    echo "</ul>";
-    $pdo = null;
-}
-catch (PDOException $e){
-    die($e->getMessage());
-  }
-}
 class DatabaseHelper { 
     /* Returns a connection object to a database */
     public static function createConnection( $values=array() ) { 
@@ -53,4 +36,21 @@ class DatabaseHelper {
     } 
    }
    
+   function songId(){
+    try{
+    
+        $pdo= new PDO(DBCONNSTRING, DBUSER, DBPASS);
+        $sql="Select song_id FROM songs";
+        $sId = $pdo-> Query($sql);
+        echo "<ul>";
+        while($row = $sId ->fetch()){
+            echo "<li>". $row['song_id'] ."</li>";
+        }
+        echo "</ul>";
+        $pdo = null;
+    }
+    catch (PDOException $e){
+        die($e->getMessage());
+      }
+    }
 ?>
