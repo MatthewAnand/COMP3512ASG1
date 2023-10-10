df4cf0674bd956627983cf6a4083ac55c730aa07
MatthewAnand <manan495@mtroyal.ca>
2023-10-10 10:24:34 -0600

    Update databasestuff.inc.php

--- includes/databasestuff.inc.php
+++ includes/databasestuff.inc.php
@@ -36,21 +36,6 @@ class DatabaseHelper {
     } 
    }
    
-   function songId(){
-    try{
-    
-        $pdo= new PDO(DBCONNSTRING, DBUSER, DBPASS);
-        $sql="Select song_id FROM songs";
-        $sId = $pdo-> Query($sql);
-        echo "<ul>";
-        while($row = $sId ->fetch()){
-            echo "<li>". $row['song_id'] ."</li>";
-        }
-        echo "</ul>";
-        $pdo = null;
-    }
-    catch (PDOException $e){
-        die($e->getMessage());
-      }
-    }
+  
+
 ?>
